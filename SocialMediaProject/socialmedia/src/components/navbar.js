09a9783c58e33ca46b7e4d16b0ffdad44fd56f43
Baturpcu <batusalkim11@gmail.com>
2023-03-18 02:02:58 +0300

    update

--- SocialMediaProject/socialmedia/src/components/navbar.js
+++ SocialMediaProject/socialmedia/src/components/navbar.js
@@ -1,10 +1,32 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
+import {useAuthState} from "react-firebase-hooks/auth";
+import { signOut } from "firebase/auth";
+
 
 export const Navbar = ()=>{
+    const [user] = useAuthState(auth);
+
+    const signUserOut = async()=>{
+        await signOut(auth);
+    }
+
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
+        <div className="navbar">
+            <div className="links">
+                <Link to="/">Home</Link>
+                <Link to="/login">Login</Link>
+            </div>
+            <div className="user">
+                {user && (
+                    <>
+                        <p>{ user?.displayName}</p>
+                        <img src={user?.photoURL} width="20" height="20" />
+                    <button onClick={signUserOut}>Log Out</button>
+                    </>
+                )}
+            </div>
         </div>
+        
     )
 }
