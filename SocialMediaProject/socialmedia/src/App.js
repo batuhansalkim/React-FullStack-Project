b5f4d213cbbb4fad555a3e8aeb315276124c88ed
Baturpcu <batusalkim11@gmail.com>
2023-03-17 21:11:45 +0300

    update

--- SocialMediaProject/socialmedia/src/App.js
+++ SocialMediaProject/socialmedia/src/App.js
@@ -1,12 +1,12 @@
 import { Route, Router, Routes } from 'react-router-dom';
 import './App.css';
-imğ
+import { Main } from './pages/main';
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/'/>
+          <Route path='/' element={<Main></Main>}/>
         </Routes>
       </Router>
     </div>
---
+++ SocialMediaProject/socialmedia/src/pages/main.js
@@ -0,0 +1,5 @@
+export const Main=()=>{
+    return(
+        <div>Anasayfa</div>  
+    )
+}
