639e5a63aa0d61667209c648e53e9231a8eb0480
Baturpcu <batusalkim11@gmail.com>
2023-03-17 21:06:51 +0300

    update

---
+++ SocialMediaProject/socialmedia/src/App.js
@@ -0,0 +1,17 @@
+import { Route, Router, Routes } from 'react-router-dom';
+import './App.css';
+imğ
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path='/'/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+  
