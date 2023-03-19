b59d72970daefa3060b986d3756d01fd8e92899b
Baturpcu <batusalkim11@gmail.com>
2023-03-20 02:00:58 +0300

    update

--- SocialMediaProject/socialmedia/src/pages/main.js
+++ SocialMediaProject/socialmedia/src/pages/main.js
@@ -1,4 +1,19 @@
+import {getDocs,collection} from "firebase/firestore";
+import {useEffect, useState} from "react";
+import {db} from "../config/firebase";
+
 export const Main=()=>{
+    const [postsList, setPostsList] = useState(null);
+    const postsRef = collection(db,"posts");
+
+    const getPosts=async ()=>{
+        const data = await getDocs(postsRef);
+        setPostsList(data.docs.map((doc)=>({...doc.data(), id:doc.id})));
+    };
+    useEffect(()=>{
+        getPosts();
+    },[]);
+    
     return(
         <div>Anasayfa</div>  
     )
