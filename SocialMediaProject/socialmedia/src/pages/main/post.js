3d4db507730f57f7855881161536dc40d2a9eaad
Baturpcu <batusalkim11@gmail.com>
2023-03-20 04:01:16 +0300

    update

--- SocialMediaProject/socialmedia/src/pages/main/post.js
+++ SocialMediaProject/socialmedia/src/pages/main/post.js
@@ -2,16 +2,20 @@ export const Post =(props)=>{
     const {post} = props;
 
     return(
-       <div>
-        <div className="title">
-            <h1>{post.title}</h1>
-        </div>
-        <div className="body">
-            <p>{post.description}</p>
-        </div>
-        <div className="footer">
-            <p>@{post.username}</p>
-        </div>
+       <div className="container">
+            {/* <div className="stick"></div> */}
+            <div className="title">
+                <h1>{post.title}</h1>
+            </div>
+            <div className="body">
+                <p>{post.description}</p>
+            </div>
+            <div className="footer">
+                <p>@{post.username}</p>
+                <button>&#128077;</button>
+                
+            </div>
+            {/* <div className="stick"></div> */}
        </div> 
-    ) 
+    ) //<img src={user?.photoURL} width="40" height="40" />
 };
