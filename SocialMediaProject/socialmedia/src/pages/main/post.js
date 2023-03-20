44e2b4ded02c8adc2f80f21307b5a33a817bfd56
Baturpcu <batusalkim11@gmail.com>
2023-03-20 16:57:10 +0300

    update

--- SocialMediaProject/socialmedia/src/pages/main/post.js
+++ SocialMediaProject/socialmedia/src/pages/main/post.js
@@ -1,6 +1,28 @@
+import { addDoc,getDocs,collection,query,where } from "firebase/firestore";
+import {useAuthState} from "react-firebase-hooks/auth";
+import {db,auth} from "../../config/firebase";
+import { useEffect, useState } from "react";
+
+
 export const Post =(props)=>{
     const {post} = props;
+    const [user] = useAuthState(auth);
+    const [likeAmount,setLikeAmount] = useState();
+    const likesRef = collection(db,"likes");
 
+    const likesDoc = query(likesRef,where("postId", "==", post.id));
+
+    const getLikes = async()=>{
+     const data =  await getDocs(likesDoc);
+     setLikeAmount(data.docs.length);
+    }
+
+    const addLike=async()=>{
+        await addDoc(likesRef, {userId:user?.uid,postId:post.id})
+    }
+    useEffect(()=>{
+        getLikes();
+    },[]);
     return(
        <div className="container">
             {/* <div className="stick"></div> */}
@@ -12,8 +34,8 @@ export const Post =(props)=>{
             </div>
             <div className="footer">
                 <p>@{post.username}</p>
-                <button>&#128077;</button>
-                
+                <button onClick={addLike}>&#128077;</button>
+                {likeAmount && <p className="likes">Likes: {likeAmount}</p>}
             </div>
             {/* <div className="stick"></div> */}
        </div> 
