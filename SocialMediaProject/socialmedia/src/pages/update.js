8898e558ae75d23aa85a3216afa1e28350fdef7c
Baturpcu <batusalkim11@gmail.com>
2023-03-21 23:59:35 +0300

    update

--- SocialMediaProject/socialmedia/src/pages/update.js
+++ SocialMediaProject/socialmedia/src/pages/update.js
@@ -4,7 +4,9 @@ export const Update=()=>{
             <h1>Yeni Güncellemeler Yüklenecek...</h1>
             <h5 style={{color:"green"}}>Postları beğenme dışında yorum yapabilme özelliği eklenecek</h5>
             <h5 style={{}}>Profil Oluşturma</h5>
-            <h5 style={{}}>Profil Oluşturma</h5>
+            <h5>İlgi alanı seçme</h5>
+            <h5 style={{}}>İlgi alanıyla ilgili Fotograf paylaşma</h5>
+            <h5>Seninle aynı ilgi alanına sahip insanlarla tanışma fırsatı</h5>
         </div>
     )
 }
